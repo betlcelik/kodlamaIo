eb0bf53c65b870183b1b2cea8755b34b30795b8f
BETUL <celikbetul313@gmail.com>
2021-08-11 17:00:44 +0300

    updated

--- kodlamaIo/src/kodlamaIo/CourseManager.java
+++
@@ -1,9 +0,0 @@
-package kodlamaIo;
-
-public class CourseManager {
-public void addCourse (Course course) {
-		
-		System.out.println("Kurs baþarýyla eklendi: "+course.name);
-	}
-
-}
--- kodlamaIo/src/kodlamaIo/InstructorManager.java
+++
@@ -1,9 +0,0 @@
-package kodlamaIo;
-
-public class InstructorManager extends UserManager{
-	
-	public void giveHomework(Course course) {
-		
-		System.out.println( course.name +" Ödevlendirme yapýlmýþtýr");
-	}
-}
